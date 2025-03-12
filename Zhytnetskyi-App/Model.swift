391fcf913112c24454ec20f363afd5d0de133767
Oleksiy Zhytnetsky <oleksii.zhytnetsky@gmail.com>
2025-03-12 12:43:52 +0200

    Complete lab 2

---
+++ Zhytnetskyi-App/Model.swift
@@ -0,0 +1,41 @@
+//
+//  Model.swift
+//  Zhytnetskyi-App
+//
+//  Created by Oleksiy Zhytnetsky on 11.03.2025.
+//
+
+import Foundation
+
+struct ExtendedPostDetails {
+    let data: PostDetails
+    let saved: Bool
+}
+
+struct ApiResponse : Codable {
+    let data: PostPaginatedInfo
+}
+
+struct PostPaginatedInfo : Codable {
+    let after: String?
+    let before: String?
+    let children: [PostData]
+}
+
+struct PostData : Codable {
+    let data: PostDetails
+}
+
+struct PostDetails : Codable {
+    let author_fullname: String
+    let domain: String
+    let title: String
+    let num_comments: Int
+    let score: Int
+    let url: String // raw img url
+    let created: Double // unix timestamp
+    
+    var cleanedUrl: String {
+        return url.replacingOccurrences(of: "&amp;", with: "&")
+    }
+}
---
+++ Zhytnetskyi-App/PostViewController.swift
@@ -0,0 +1,90 @@
+//
+//  PostViewController.swift
+//  Zhytnetskyi-App
+//
+//  Created by Oleksiy Zhytnetsky on 11.03.2025.
+//
+
+import UIKit
+import SDWebImage
+
+class PostViewController: UIViewController {
+    
+    @IBOutlet private weak var userNameLabel: UILabel!
+    @IBOutlet private weak var timeSincePostLabel: UILabel!
+    @IBOutlet private weak var domainLabel: UILabel!
+    @IBOutlet private weak var postTitleLabel: UILabel!
+    @IBOutlet private weak var bookmarkBtn: UIButton!
+    @IBOutlet private weak var upvoteBtn: UIButton!
+    @IBOutlet private weak var commentsBtn: UIButton!
+    @IBOutlet private weak var postImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Task {
+            do {
+                let resp = try await PostApiClient.fetchPosts(
+                    subreddit: "ios"
+                )
+                
+                guard let postData = resp.data.children.first else {
+                    print("Error: No post available")
+                    return
+                }
+                let post = ExtendedPostDetails(
+                    data: postData.data,
+                    saved: Bool.random()
+                )
+                
+                DispatchQueue.main.async {
+                    self.userNameLabel.text = post.data.author_fullname
+                    self.timeSincePostLabel.text = Utils.formatTimeSincePost(
+                        post.data.created
+                    )
+                    self.domainLabel.text = post.data.domain
+                    self.postTitleLabel.text = post.data.title
+                    self.upvoteBtn.setTitle(
+                        Utils.formatNumCount(post.data.score),
+                        for: .normal
+                    )
+                    self.commentsBtn.setTitle(
+                        Utils.formatNumCount(post.data.num_comments),
+                        for: .normal
+                    )
+                }
+                
+                postImageView.sd_setImage(
+                    with: URL(string: post.data.cleanedUrl),
+                    placeholderImage: UIImage(named: "placeholder.png")
+                )
+                
+                if (post.saved) {
+                    toggleBtnImg(self.bookmarkBtn)
+                }
+            }
+            catch {
+                print("Error fetching posts: \(error)")
+            }
+        }
+    }
+    
+    @IBAction
+    func bookmarkBtnTapped(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        toggleBtnImg(sender)
+    }
+    
+    private func toggleBtnImg(_ btn: UIButton) {
+        var imgName: String = "bookmark"
+        if (btn.isSelected) {
+            imgName.append(".fill")
+        }
+        
+        btn.setImage(
+            UIImage(systemName: imgName),
+            for: .normal
+        )
+    }
+    
+}
